2285cb7bab79a4fd14cbad208d43f0f0d85fbf78
krishang118 <krishangsh118@gmail.com>
2025-06-24 00:18:43 +0530

    Update

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os          
-from .utils import calculate_technical_indicators
+from src.utils import calculate_technical_indicators
 import logging
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split, TimeSeriesSplit
